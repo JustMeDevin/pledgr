eb0bbfc1d6f55d295b2872fb99215e1959ab6eac
Devin Davies <dda40@uclive.ac.nz>
2017-08-19 11:51:01 +1200

    uncomment app.js

--- project/app.js
+++ project/app.js
@@ -1,22 +1,22 @@
-//
-// const userRoutes = require('./users/users.routes')
-// const projectRoutes = require('./projects/projects.routes')
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const app = express();
-// const urlencodedParser = bodyParser.urlencoded({ extended: false });
-// const databaseConnection = require('./conf/db-connection');
-// databaseConnection.createDatabase();
-// app.use(bodyParser.json());
-//
-// app.use(function (req, res, next) {
-//   res.setHeader('Content-Type', 'application/json');
-//   next();
-// });
-//
-// app.use('/users', userRoutes);
-// app.use('/projects', projectRoutes);
-//
-// app.listen(4941, function () {
-//     console.log('listening on port: ' + 4941);
-// });
+
+const userRoutes = require('./users/users.routes')
+const projectRoutes = require('./projects/projects.routes')
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+const databaseConnection = require('./conf/db-connection');
+databaseConnection.createDatabase();
+app.use(bodyParser.json());
+
+app.use(function (req, res, next) {
+  res.setHeader('Content-Type', 'application/json');
+  next();
+});
+
+app.use('/users', userRoutes);
+app.use('/projects', projectRoutes);
+
+app.listen(4941, function () {
+    console.log('listening on port: ' + 4941);
+});
