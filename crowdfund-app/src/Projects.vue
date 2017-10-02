42b93167e90b1e542f886627eda5845cfacd720e
Devin Davies <dda40@uclive.ac.nz>
2017-10-02 20:14:50 +1300

    user can select a project and get some extra info

--- crowdfund-app/src/Projects.vue
+++ crowdfund-app/src/Projects.vue
@@ -3,13 +3,32 @@
         <div v-if="errorFlag" style="color: red;">
             {{ error }}
         </div>
+
+        <div id="details-wrapper">
+            <div v-if="selectedProject !== null" id="project-details">
+                <h3>{{ selectedProject.title }}</h3>
+                <h4>{{ selectedProject.subtitle }}</h4>
+
+                <div id="img-wrapper-pDetails">
+                    <img img :src="getImage(selectedProject)" v-bind:alt="selectedProject" onerror="this.style.display='none'">
+                </div>
+
+                <p id="project-description"> {{ selectedProject.description }}</p>
+
+                <h5> Creators </h5>
+                <p v-for="creator in selectedProject.creators"> {{ creator.username }} </p>
+
+
+            </div>
+        </div>
+
         <div id="searchWrap">
-            <input class="search" type="text" v-model="search" placeholder=""/>
+            <input v-if="!isSelected" class="search" id="search-bar" type="text" v-model="search" placeholder=""/>
         </div>
 
         <div id="projects-wrapper">
-            <div id="projects" v-for="project in searchedProjects">
-                <div class="project float">
+            <div id="projects" v-if="!isSelected" v-for="project in searchedProjects" >
+                <div v-bind:id="project.id" class="project float" v-on:click="getProject(project)">
                     <div id="img-wrapper">
                         <img img :src="getImage(project)" v-bind:alt="project" onerror="this.style.display='none'">
                     </div>
@@ -19,7 +38,6 @@
                 </div>
             </div>
         </div>
-
     </div>
 </template>
 
@@ -32,7 +50,9 @@
                 search: '',
                 error: "",
                 errorFlag: false,
-                projects: []
+                projects: [],
+                selectedProject: null,
+                isSelected: false
             }
         },
         mounted: function() {
@@ -51,8 +71,24 @@
 
             getImage: function(project){
                 return config.apiUrl + "projects/" + project.id + "/image";
-            }
+            },
+
+            getProject: function(project){
+                this.$http.get(config.apiUrl + "projects/" + project.id)
+                    .then(function(response){
+                        this.selectedProject = response.data;
+                        this.hideProjects();
+                    }, function(error) {
+                        this.error = error;
+                        this.errorFlag = true;
+                    });
+            },
+
+            hideProjects: function(){
+                this.isSelected = !this.isSelected;
+            },
         },
+
         computed: {
             searchedProjects: function() {
                 var self = this;
@@ -68,7 +104,7 @@
                         }
                         return exists;
                     });
-            }
+            },
         }
     }
 </script>
