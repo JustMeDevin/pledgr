6499c3f8f3070c7a9798cc2f6d2439f12a9a7512
Devin Davies <dda40@uclive.ac.nz>
2017-10-02 20:48:33 +1300

    added window for log in

--- crowdfund-app/src/App.vue
+++ crowdfund-app/src/App.vue
@@ -1,5 +1,42 @@
 <template>
   <div id="app">
+
+    <div v-if="visible" id="log-in-form">
+
+    </div>
+
+    <div id="header">
+      <h1 id="header-title" class="unselectable">Pledgr</h1>
+      <button v-on:click='visible = !visible' v-if="!isLoggedIn" id="log-in">Log in</button>
+    </div>
+
     <router-view></router-view>
   </div>
 </template>
+
+<script>
+    import { config } from './config';
+
+    export default {
+        data(){
+            return{
+                search: '',
+                error: "",
+                errorFlag: false,
+                isLoggedIn: false,
+                visible: true
+            }
+        },
+        mounted: function() {
+
+        },
+        methods: {
+
+        },
+
+        computed: {
+
+        }
+    }
+</script>
+
