7a50595bd3310f5475bb58a6e72ca16c8f157f7f
Devin Davies <dda40@uclive.ac.nz>
2017-10-03 11:34:57 +1300

    added boxes for log in and blur effect

--- crowdfund-app/src/App.vue
+++ crowdfund-app/src/App.vue
@@ -2,8 +2,21 @@
   <div id="app">
 
     <div v-if="visible" id="log-in-form">
+      <form class="login-form" v-on:submit.prevent="loginUser" id="form">
+        <fieldset>
 
+          <input class="input" id="username" v-model="user.username" type="text" placeholder="Username">
+
+          <input class="input" id="password" v-model="user.password" type="password" placeholder="Password">
+
+          <button id="log-in-button" type="submit" class="pledgr-button">Log In</button>
+          <p v-if="submitData">{{submitData|json}}</p>
+
+        </fieldset>
+      </form>
     </div>
+
+  <div v-on:click="hideLoginWindow" v-bind:class="[visible ? 'window-open' : '', 'window-close']" class="fade-window"></div>
 
     <div id="header">
       <h1 id="header-title" class="unselectable">Pledgr</h1>
@@ -24,14 +37,24 @@
                 error: "",
                 errorFlag: false,
                 isLoggedIn: false,
-                visible: true
+                visible: false,
+                user: {
+                    username: null,
+                    password: null,
+                },
+                submitData: null
             }
         },
         mounted: function() {
-
         },
         methods: {
+            loginUser: function() {
+                this.submitData = this.user;
+            },
 
+            hideLoginWindow: function(){
+                this.visible = false;
+            }
         },
 
         computed: {
