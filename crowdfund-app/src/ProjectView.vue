c745518a6a60e412a9565bded617546a55a01322
Devin Davies <dda40@uclive.ac.nz>
2017-10-04 23:26:10 +1300

    added more information to the project details panel

--- crowdfund-app/src/ProjectView.vue
+++ crowdfund-app/src/ProjectView.vue
@@ -1,7 +1,9 @@
 <template>
     <div id="details-wrapper">
         <div id="project-details">
-            <router-link :to="{name: 'projects'}" tag="button" id="back-to-projects-button" class="pledgr-button-bold button-float">back</router-link>
+            <div id="project-button-bar">
+                <router-link :to="{name: 'projects'}" tag="button" id="back-to-projects-button" class="pledgr-button-bold button-float">back</router-link>
+            </div>
             <div id="img-wrapper-pDetails">
                 <img img :src="getImage(selectedProject)" v-bind:alt="selectedProject" onerror="this.style.display='none'">
             </div>
@@ -11,14 +13,31 @@
 
             <p id="project-description"> {{ selectedProject.description }}</p>
 
-            <h5> Creators </h5>
-            <p id="creator-names" v-for="creator in selectedProject.creators"> {{ creator.username }} </p>
+            <h5 id="rewards-header">Rewards</h5>
+            <div id="rewards" v-for="reward in selectedProject.rewards">
+                <reward :reward="reward"></reward>
+            </div>
+
+            <div id="backers-wrapper">
+                <h5>Backers</h5>
+            </div>
+
+            <div id="creators-wrapper">
+                <h5> Creators </h5>
+                <div id="circle-wrappers">
+                    <div v-for="creator in selectedProject.creators" id="creator-div">
+                        <p id="creator-names">{{creator.username}}</p>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
 </template>
 
 <script>
     import { config } from './config';
+    import Reward from './Reward.vue'
 
     export default {
         data(){
@@ -28,13 +47,18 @@
                 errorFlag: false,
                 projects: [],
                 selectedProject: null,
+                progressValue: 0.5
             }
         },
+        components: {
+            Reward
+        },
+
         mounted: function() {
             this.getProject();
         },
         methods: {
-            getImage: function(project){
+            getImage: function(){
                 return config.apiUrl + "projects/" + this.$route.params.projectId + "/image";
             },
 
@@ -42,6 +66,7 @@
                 this.$http.get(config.apiUrl + "projects/" + this.$route.params.projectId)
                     .then(function(response){
                         this.selectedProject = response.data;
+                        console.log(this.selectedProject);
                     }, function(error) {
                         this.error = error;
                         this.errorFlag = true;
---
+++ crowdfund-app/src/Reward.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="bigdaddy-wrapper">
+        <div id="reward-wrapper">
+            <div id="reward-shape" class="float">
+                <p id="reward-price"> Pledge ${{ reward.amount}}</p>
+                <p id="reward-description"> {{ reward.description }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import { config } from './config';
+
+    export default {
+        props: ['reward'],
+        data(){
+            return{
+            }
+        },
+
+        mounted: function() {
+
+        },
+        methods: {
+
+        },
+
+        computed: {
+        }
+    }
+</script>
+
+
+
+
+
